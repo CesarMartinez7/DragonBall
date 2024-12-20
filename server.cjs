b14cd1fcf0b0854cf15ad635f56a4f443b571608
CesarMartinez <cesarwamartinez@gmail.com>
2024-12-20 04:38:27 -0500

    add-params y useparams of react-router dom for item.id and react router /characters/:id

--- server.cjs
+++ server.cjs
@@ -2,14 +2,16 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
