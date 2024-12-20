b25a12386efa190ab15b62e046b7ea934f8cd7d7
CesarMartinez <cesarwamartinez@gmail.com>
2024-12-20 02:00:12 -0500

    inicilizate

--- src/Components/Esferas.jsx
+++ src/Components/Esferas.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Esfera1 from "../assets/Esfera1.png";
-import Esfera2 from "../assets/Esfera2.png";
-import Esfera3 from "../assets/Esfera3.png";
-import Esfera4 from "../assets/Esfera4.png";
-import Esfera5 from "../assets/Esfera5.png";
-import Esfera6 from "../assets/Esfera6.png";
-import Esfera7 from "../assets/Esfera7.png";
+import Esfera1 from "../assets/esfera1.png";
+import Esfera2 from "../assets/esfera2.png";
+import Esfera3 from "../assets/esfera3.png";
+import Esfera4 from "../assets/esfera4.png";
+import Esfera5 from "../assets/esfera5.png";
+import Esfera6 from "../assets/esfera6.png";
+import Esfera7 from "../assets/esfera7.png";
 import "./Esferas.css";
 
 const Esferas = () => {
