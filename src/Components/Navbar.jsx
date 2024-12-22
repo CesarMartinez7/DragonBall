db92143bb4260b291ddaba4c09cda640c66a0296
CesarMartinez <cesarwamartinez@gmail.com>
2024-12-22 17:31:32 -0500

    add-navbar-burger

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,41 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Dragon from "../assets/dragonball-z.svg";
 import { Link } from "react-router-dom";
 
-
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav class="navbar rounded-box shadow card-glass border-none text-neutral-content border-md z-50">
-      <div class="w-full md:flex md:items-center md:gap-2">
-        <div class="flex items-center justify-between">
-          <div class="navbar-start items-center justify-between max-md:w-full">
+    <nav className="navbar rounded-box shadow card-glass border-none text-neutral-content border-md z-50">
+      <div className="w-full md:flex md:items-center md:gap-2">
+        <div className="flex items-center justify-between">
+          <div className="navbar-start items-center justify-between max-md:w-full">
             <a href="#" aria-label="Homepage Link">
-              <img src={Dragon} className="scale-[4]"/>
+              <img src={Dragon} className="scale-[2] ml-3" />
             </a>
-            <div class="md:hidden">
+            <div className="md:hidden">
               <button
                 type="button"
-                class="collapse-toggle btn btn-outline btn-secondary btn-sm btn-square"
-                data-collapse="#logo-navbar-collapse"
+                className="collapse-toggle btn btn-outline btn-secondary btn-sm btn-square"
+                onClick={toggleMenu}
                 aria-controls="logo-navbar-collapse"
                 aria-label="Toggle navigation"
               >
-                <span class="icon-[tabler--menu-2] collapse-open:hidden size-4"></span>
-                <span class="icon-[tabler--x] collapse-open:block hidden size-4"></span>
+                <span className={`icon-[tabler--menu-2] ${isOpen ? 'hidden' : 'block'} size-4`}></span>
+                <span className={`icon-[tabler--x] ${isOpen ? 'block' : 'hidden'} size-4`}></span>
               </button>
             </div>
           </div>
         </div>
         <div
           id="logo-navbar-collapse"
-          class="md:navbar-end collapse hidden grow basis-full overflow-hidden transition-[height] duration-300 max-md:w-full"
+          className={`md:navbar-end collapse ${isOpen ? 'block' : 'hidden'} grow basis-full overflow-hidden transition-[height] duration-300 max-md:w-full`}
         >
-          <ul class="menu md:menu-horizontal gap-2 card-glass p-0 max-md:mt-2">
+          <ul className="menu md:menu-horizontal gap-2 card-glass p-0 max-md:mt-2">
             <li>
-              <a href="#" className="card-glass">Home</a>
+              <Link to="/" className="card-glass">Home</Link>
             </li>
             <li>
-              <Link to={"esferas"} >About</Link>
+              <Link to="/esferas" className="card-glass">Esferas</Link>
             </li>
           </ul>
         </div>
